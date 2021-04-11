202c93fff818de926e18f2a5d10d9a2d222d3c51
Fred Bogdanoff <fredericks.webdesigns@gmail.com>
2021-04-11 11:22:23 +1000

    fixed fontawesome issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-fontawesome-css`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
