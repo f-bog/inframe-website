994776d203ba6e81d254e8d6087c6c7e50684ebd
Frederick Bogdanoff <fredericks.webdesigns@gmail.com>
2022-09-04 14:51:58 +1000

    new commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <StyledLayout>
         <main>{children}</main>
-        <Footer />
+        {/* <Footer /> */}
       </StyledLayout>
     </>
   )
